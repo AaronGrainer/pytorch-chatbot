fd51ecf7f0bc680eacb2ef9f045aba197d1d7493
Aaron Grainer <aaronlimfz@gmail.com>
2021-06-09 20:32:24 +0800

    Reformatted

--- helpers.py
+++ helpers.py
@@ -2,40 +2,44 @@ import unicodedata
 import re
 
 def printLines(file, n=10):
-  with open(file, 'rb') as datafile:
-    lines = datafile.readlines()
-  for line in lines[:n]:
-    print(line)
+    with open(file, 'rb') as datafile:
+        lines = datafile.readlines()
+    for line in lines[:n]:
+        print(line)
 
 
-# Turn a Unicode string to plain ASCII, thanks to
-# http://stackoverflow.com/a/518232/2809427
 def unicodeToAscii(s):
-  return ''.join(
-    c for c in unicodedata.normalize('NFD', s)
-    if unicodedata.category(c) != 'Mn'
-  )
+    """Turn a Unicode string to plain ASCII, thanks to
+    http://stackoverflow.com/a/518232/2809427
+    """
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
 
 
-# Lowercase, trim, and remove non-letter characters
 def normalizeString(s):
-  s = unicodeToAscii(s.lower().strip())
-  s = re.sub(r"([.!?])", r" \1", s)
-  s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
-  s = re.sub(r"\s+", r" ", s).strip()
-  return s
+    """Lowercase, trim, and remove non-letter characters
+    """
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    s = re.sub(r"\s+", r" ", s).strip()
+    return s
 
 
-# Returns True iff both sentences in a pair 'p' are under the MAX_LENGTH threshold
 def filterPair(p, max_length):
-  # Input sequences need to preserve the last word for EOS token
-  return len(p[0].split(' ')) < max_length and len(p[1].split(' ')) < max_length
+    """Returns True iff both sentences in a pair 'p' are under the MAX_LENGTH threshold\
+    """
+    # Input sequences need to preserve the last word for EOS token
+    return len(p[0].split(' ')) < max_length and len(p[1].split(' ')) < max_length
 
 
-# Filter pairs using filterPair condition
 def filterPairs(pairs, max_length):
-  return [pair for pair in pairs if filterPair(pair, max_length)]
+    """Filter pairs using filterPair condition
+    """
+    return [pair for pair in pairs if filterPair(pair, max_length)]
 
 
 def indexesFromSentence(voc, sentence, eos_token):
-  return [voc.word2index[word] for word in sentence.split(' ')] + [eos_token]
+    return [voc.word2index[word] for word in sentence.split(' ')] + [eos_token]
--- process.py
+++ process.py
@@ -2,68 +2,72 @@ import csv
 import codecs
 
 class Preprocess:
-  def __init__(self, datafile, linefile, conversationfile, lineFields, conversationFields):
-    self.datafile = datafile
-    self.linefile = linefile
-    self.conversationfile = conversationfile
-    self.lineFields = lineFields
-    self.conversationFields = conversationFields
+    def __init__(self, datafile, linefile, conversationfile, lineFields, conversationFields):
+        self.datafile = datafile
+        self.linefile = linefile
+        self.conversationfile = conversationfile
+        self.lineFields = lineFields
+        self.conversationFields = conversationFields
 
-  # Splits each line of the file into a dictionary of fields
-  def loadLines(self):
-    print("\nProcessing corpus...")
-    self.lines = {}
-    with open(self.linefile, 'r', encoding='iso-8859-1') as f:
-      for line in f:
-        values = line.split(" +++$+++ ")
-        # Extract fields
-        lineObj = {}
-        for i, field in enumerate(self.lineFields):
-          lineObj[field] = values[i]
-        self.lines[lineObj['lineID']] = lineObj
+    def loadLines(self):
+        """Splits each line of the file into a dictionary of fields
+        """
+        print("\nProcessing corpus...")
+        self.lines = {}
+        with open(self.linefile, 'r', encoding='iso-8859-1') as f:
+            for line in f:
+                values = line.split(" +++$+++ ")
+                # Extract fields
+                lineObj = {}
+                for i, field in enumerate(self.lineFields):
+                    lineObj[field] = values[i]
+                self.lines[lineObj['lineID']] = lineObj
 
-  # Groups fields of lines from `loadLines` into conversations based on *movie_conversations.txt*
-  def loadConversations(self):
-    print("\nLoading conversations...")
-    self.conversations = []
-    with open(self.conversationfile, 'r', encoding='iso-8859-1') as f:
-      for line in f:
-        values = line.split(" +++$+++ ")
-        # Extract fields
-        convObj = {}
-        for i, field in enumerate(self.conversationFields):
-          convObj[field] = values[i]
-        # Convert string to list (convObj["utteranceIDs"] == "['L598485', 'L598486', ...]")
-        lineIds = eval(convObj["utteranceIDs"])
-        # Reassemble lines
-        convObj["lines"] = []
-        for lineId in lineIds:
-          convObj["lines"].append(self.lines[lineId])
-        self.conversations.append(convObj)
+    def loadConversations(self):
+        """Groups fields of lines from `loadLines` into conversations based on *movie_conversations.txt*
+        """
+        print("\nLoading conversations...")
+        self.conversations = []
+        with open(self.conversationfile, 'r', encoding='iso-8859-1') as f:
+            for line in f:
+                values = line.split(" +++$+++ ")
+                # Extract fields
+                convObj = {}
+                for i, field in enumerate(self.conversationFields):
+                    convObj[field] = values[i]
+                # Convert string to list (convObj["utteranceIDs"] == "['L598485', 'L598486', ...]")
+                lineIds = eval(convObj["utteranceIDs"])
+                # Reassemble lines
+                convObj["lines"] = []
+                for lineId in lineIds:
+                    convObj["lines"].append(self.lines[lineId])
+                self.conversations.append(convObj)
 
-  # Extracts pairs of sentences from conversations
-  def extractSentencePairs(self):
-    qa_pairs = []
-    for conversation in self.conversations:
-      # Iterate over all the lines of the conversation
-      # We ignore the last line (no answer for it)
-      for i in range(len(conversation["lines"]) - 1):
-        inputLine = conversation["lines"][i]["text"].strip()
-        targetLine = conversation["lines"][i+1]["text"].strip()
-        # Filter wrong samples (if one of the lists is empty)
-        if inputLine and targetLine:
-          qa_pairs.append([inputLine, targetLine])
-    return qa_pairs
+    def extractSentencePairs(self):
+        """Extracts pairs of sentences from conversations
+        """
+        qa_pairs = []
+        for conversation in self.conversations:
+            # Iterate over all the lines of the conversation
+            # We ignore the last line (no answer for it)
+            for i in range(len(conversation["lines"]) - 1):
+                    inputLine = conversation["lines"][i]["text"].strip()
+                    targetLine = conversation["lines"][i+1]["text"].strip()
+                    # Filter wrong samples (if one of the lists is empty)
+                    if inputLine and targetLine:
+                        qa_pairs.append([inputLine, targetLine])
+        return qa_pairs
 
-  # Write new csv file
-  def writeCSV(self):
-    print("\nWriting newly formatted file...")
-    delimiter = '\t'
-    delimiter = str(codecs.decode(delimiter, "unicode_escape"))
-    with open(self.datafile, 'w', encoding='utf-8', newline='') as outputfile:
-      writer = csv.writer(outputfile, delimiter=delimiter)
-      for pair in self.extractSentencePairs():
-        writer.writerow(pair)
+    def writeCSV(self):
+        """Write new csv file
+        """
+        print("\nWriting newly formatted file...")
+        delimiter = '\t'
+        delimiter = str(codecs.decode(delimiter, "unicode_escape"))
+        with open(self.datafile, 'w', encoding='utf-8', newline='') as outputfile:
+            writer = csv.writer(outputfile, delimiter=delimiter)
+            for pair in self.extractSentencePairs():
+                writer.writerow(pair)
 
 
 
